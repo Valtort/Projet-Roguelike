188b50f159ed7dd58e20bbb30c4601425c6c4138
Jules Le Prince <juleslp35740@gmail.com>
2025-11-12 13:51:32 +0100

    added comments and types

--- src/utils.ml
+++ src/utils.ml
@@ -19,10 +19,12 @@ let move (old_position : int * int) (new_position : int * int) : int * int =
   | _ -> old_position
 
 
-let correct_coordinates (x, y) : bool =
+(** [correct_coordinates coord] renvoi [true] si les coordonnées coord sont
+valides, et [false] sinon *)
+let correct_coordinates ((x, y): int * int) : bool =
   (0 <= x) && (x < width) && (0 <= y) && (y < height);;
 
-(* Renvoie une direction aléatoire *)
+(** [random_direction ()] renvoi une direction aléatoire *)
 let random_direction () : int * int =
   let random_dir = Random.int 4 in
   match random_dir with
