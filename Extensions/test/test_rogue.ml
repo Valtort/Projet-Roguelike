59ce713da772ecdb8591e8cd1aa1099e1093e83e
Valtort <valtort@hotmail.com>
2025-11-14 14:19:24 +0100

    Tests pour l'extentsion 2 et 3

--- Extensions/test/test_rogue.ml
+++ Extensions/test/test_rogue.ml
@@ -3,12 +3,23 @@ open World
 open Utils
 open Elephant
 open Spider
+open Sandbox
+
+(** [reset_world ()] reinitialse le monde *)
+let reset_world () : unit = 
+  for i = 0 to width - 1 do 
+    for j = 0 to height - 1 do 
+      world.(i).(j) <- Empty;
+    done;
+  done
+
 
 (*-------------------------------------------*)
 (*------ Test pour le fichier world.ml ------*)
 (*-------------------------------------------*)
 
 let test_world () =
+  reset_world ();
   set (10,20) Camel;
   assert(get (10,20) = Camel && world.(10).(20) = Camel);
   assert(get (width+1, height+1) = Cactus)
@@ -18,6 +29,7 @@ let test_world () =
 (*-------------------------------------------*)
 
 let test_utils () = 
+  reset_world ();
   assert((2,4) ++ (5,7) = (7,11));
   set (10,20) Camel;
   let _ = move (10,20) (20,20) in
@@ -36,6 +48,7 @@ let test_utils () =
 (*-------------------------------------------*)
 
 let test_elephant () = 
+  reset_world ();
   set (10,20) Camel;
   assert(dir_to_camel (10,10) = (0,1));
   set (10, 15) Cactus;
@@ -49,6 +62,7 @@ let test_elephant () =
 (*-------------------------------------------*)
 
 let test_spider () = 
+  reset_world ();
   set (10,10) Cactus;
   set (8,10) Cactus;
   assert(adjacent_empty_cells (9,10) = [|(9,9); (9,11)|]);
@@ -56,6 +70,45 @@ let test_spider () =
   assert(adjacent_empty_cells (9,10) = [|(9,9); (8,10);(9,11)|]);
   set (10,10) Empty;
   assert(adjacent_empty_cells (9,10) = [|(10,10);(9,9); (8,10);(9,11)|])
+
+(*-------------------------------------------*)
+(*----- Test pour le fichier sandbox.ml------*)
+(*-------------------------------------------*)
+(*               Extension 3                 *)
+let test_sandbox () = 
+  reset_world ();
+  current_position := (0,0);
+  set (0,0) Cross;
+  last_seen := Empty;
+  move_cross (10,10);
+  assert(get (0,0) = Empty);
+  assert(get (10,10) = Cross);
+  set (20,20) Cactus;
+  move_cross (20,20);
+  assert(get (20,20) = Cross);
+  move_cross (25,25);
+  assert(get (25,25) = Cross);
+  assert(get (20,20) = Cactus)
+ 
+(*-------------------------------------------*)
+(*-----  Test pour le fichier utils.ml ------*)
+(*-------------------------------------------*)
+(*               Extension 2                 *)
+
+let test_utils2 () = 
+  reset_world ();
+  set (10,10) Cactus;
+  set (11,11) Camel;
+  set (11,12) Camel;
+  set (11,13) Camel;
+  set (0,12) Camel;
+  set (1,12) Cookie;
+  let pos_l = get_camel_pos () in
+  assert (List.mem (11,11) pos_l);
+  assert (List.mem (11,12) pos_l);
+  assert (List.mem (11,13) pos_l);
+  assert (List.mem (0,12) pos_l);
+  assert (not (List.mem (10,10) pos_l))
 
 let () = 
   test_world();
@@ -66,4 +119,8 @@ let () =
   print_string"Test elephant : réussi !\n";
   test_spider();
   print_string"Test spider : réussi !\n";
+  test_sandbox();
+  print_string"Test sandbox : réussi !\n";
+  test_utils2();
+  print_string"Test extension 2 : réussi !\n";
   print_string"Tous les tests ont réussis !\n"
